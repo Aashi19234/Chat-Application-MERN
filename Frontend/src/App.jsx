dea5d4850d5958dd52b82b70382c7a5395853ec0
Aashi19234 <aasthaj375@gmail.com>
2025-02-21 13:55:47 +0530

    Frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Left from './home/left part/Left'
+import Right from './home/right part/Right'
+
+function App() {
+  return (
+    <div  className='flex h-screen'>
+   <Left/>
+   <Right/>
+   </div>
+  )
+}
+
+export default App
---
+++ Frontend/src/home/left part/Logout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { BiLogOutCircle } from "react-icons/bi";
+
+function Logout() {
+    return (
+        <div className='h-[10vh'>
+           <BiLogOutCircle />
+        </div>
+    )
+}
+
+export default Logout
