b4d417090dfe04f7296ff9ce31863133e80c1218
Aashi19234 <aasthaj375@gmail.com>
2025-03-29 22:23:29 +0530

    Cors origin changed

--- Backend/Frontend/src/context/Socketcontext.jsx
+++ Backend/Frontend/src/context/Socketcontext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider=({children})=>{
 
     useEffect(()=>{
         if(authuser){
-            const socket=io("http://localhost:4000",{
+            const socket=io("https://chat-application-mern-2.onrender.com",{
                 query:{
                     userid:authuser.user._id
                 },
